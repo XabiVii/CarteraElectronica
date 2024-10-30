1592d9b3bc381ddb73771db68ae14a13e200e1d8
XabiVii <xabier.villa@opendeusto.es>
2024-10-30 13:27:11 +0100

    resolucion

--- src/main/MainCartera.java
+++ src/main/MainCartera.java
@@ -7,7 +7,7 @@ public class MainCartera {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		VentanaPrincipal cartera=new VentanaPrincipal();7
+		VentanaPrincipal cartera=new VentanaPrincipal();
 		
 		
 	}
