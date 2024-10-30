39787041895f73a375433175f172034842e2bf67
Cesar-Veiga <cesar.veiga@opendeusto.es>
2024-10-30 11:43:05 +0100

    creacion panel de ingresos

--- CarteraElectronica/src/gui/FormularioIngresos.java
+++ CarteraElectronica/src/gui/FormularioIngresos.java
@@ -4,4 +4,10 @@ import javax.swing.JPanel;
 
 public class FormularioIngresos extends  JPanel{
 	
+	public FormularioIngresos() {
+		super();
+		setSize(1200,750);
+		setVisible(true);
+		setLocation(null);
+	}
 }
