f97612a0bb75fbf633a9820779396ab536d5dce5
Cesar-Veiga <cesar.veiga@opendeusto.es>
2024-10-31 19:16:54 +0100

    avanzando

--- src/gui/FormularioIngresos.java
+++ src/gui/FormularioIngresos.java
@@ -1,33 +1,43 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
-public class FormularioIngresos extends  JPanel{
+public class FormularioIngresos extends  JFrame{
 	
 	public FormularioIngresos() {
 		super();
 		
 		setSize(1200,750);
-		setLayout(null);
 		setVisible(true);
-		setLocation(null);
 		
 		// Fondo y decoración
 		setBackground(Color.white);
-		setBorder(BorderFactory.createLineBorder(Color.black));
 		
 		JButton realizar = new JButton();
-		JTextField cantidad = new JTextField();
-		Container contentPane = getRootPane();
-		contentPane.add(new JLabel("Cantidad: "));
+
 		
 		// Spring Layout
+		SpringLayout spring = new SpringLayout();
+		
+		// Componentes del Layout
+		JTextField cantidad = new JTextField();
+		JLabel label = new JLabel("Cantidad: ");
+		
+		spring.addLayoutComponent(cantidad, label);
+		
+		// Layout principal
+		
+		BorderLayout principal = new BorderLayout();
+		// principal.addLayoutComponent(spring, BorderLayout.CENTER);
 	}
 }
