07efa73e9fb11310d70331ed008d396619fc6443
Cesar-Veiga <cesar.veiga@opendeusto.es>
2024-10-31 16:38:33 +0100

    Comienzo FormularioIngresos

--- src/gui/FormularioIngresos.java
+++ src/gui/FormularioIngresos.java
@@ -1,14 +1,29 @@
 package gui;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class FormularioIngresos extends  JPanel{
 	
 	public FormularioIngresos() {
 		super();
+		
 		setSize(1200,750);
+		setLayout(null);
 		setVisible(true);
 		setLocation(null);
 		
+		// Fondo y decoración
+		setBackground(Color.white);
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		JButton realizar = new JButton();
+		JTextField cantidad = new JTextField();
+		
+		// Spring Layout
 	}
 }
