36d3234dbd37ef4fcced492811a3e038f38e01f1
AlvarodelaHerran <alvarodela.herran@opendeusto.es>
2024-11-06 15:20:51 +0100

    Creacion del usuario

---
+++ src/gui/CreacionUsuario.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class CreacionUsuario extends JPanel{
+	 private static final long serialVersionUID = 1L;
+	    
+	    private JTextField Inombre, Iapellido, Iid, Icorreo, Icontrasena;
+	    private JButton aceptar, cancelar;
+	    private Usuario usuario;
+	    
+	    
+}
