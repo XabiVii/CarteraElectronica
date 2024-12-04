1627c76c12207db0a11018e4d099ff9dd6970339
Cesar-Veiga <cesar.veiga@opendeusto.es>
2024-12-04 12:38:40 +0100

    Getters and Setters

--- src/gui/Operacion.java
+++ src/gui/Operacion.java
@@ -6,6 +6,58 @@ public class Operacion {
 	String fecha;
 	String descripción;
 	String metodoPago;
-	String tipoComida;
+	String tipoPago;
+	
+	// Vamos con los getters
+	
+	public String getTipoOperacion() {
+		return tipoOperacion;
+	}
+	
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	
+	public String getFecha() {
+		return fecha;
+	}
+	
+	public String getDescripción() {
+		return descripción;
+	}
+	
+	public String getMetodoPago() {
+		return metodoPago;
+	}
+	
+	public String getTipoPago() {
+		return tipoPago;
+	}
+	
+	// Empezamos con los setters
+	
+	public void setTipoOperacion(String tipoOperacion) {
+		this.tipoOperacion = tipoOperacion;
+	}
+	
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	
+	public void setDescripción(String descripción) {
+		this.descripción = descripción;
+	}
+	
+	public void setMetodoPago(String metodoPago) {
+		this.metodoPago = metodoPago;
+	}
+	public void setTipoPago(String tipoPago) {
+		this.tipoPago = tipoPago;
+	}
+		
 }
 
