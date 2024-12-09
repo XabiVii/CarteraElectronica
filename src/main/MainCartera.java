fed86e6f2dbc2f3fbef84e1737b21a7a9bab18c1
AlvarodelaHerran <alvarodela.herran@opendeusto.es>
2024-12-09 18:09:04 +0100

    bd en main

--- src/main/MainCartera.java
+++ src/main/MainCartera.java
@@ -1,13 +1,16 @@
 package main;
 
 import javax.swing.SwingUtilities;
+
 import gui.VentanaPrincipal;
+import persistencia.GestorBD;
 
 public class MainCartera {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		GestorBD gestorBD = new GestorBD();
+		gestorBD.crearTablas();
 		SwingUtilities.invokeLater(() -> new VentanaPrincipal());
 
 		
