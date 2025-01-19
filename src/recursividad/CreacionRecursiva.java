83e5d9bdb79cd73f660a77a647548783881513f2
AlvarodelaHerran <alvarodela.herran@opendeusto.es>
2025-01-19 14:12:10 +0100

    recursividad

--- src/recursividad/CreacionRecursiva.java
+++ src/recursividad/CreacionRecursiva.java
@@ -1,5 +1,24 @@
 package recursividad;
 
+import java.io.File;
+import java.io.IOException;
+
 public class CreacionRecursiva {
+	public void hacerCopiaDeSeguridad(File carpetaOriginal, File carpetaBackup) throws IOException {
+	    File[] archivos = carpetaOriginal.listFiles();
+	    if (archivos != null) {
+	        for (File archivo : archivos) {
+	            if (archivo.isDirectory()) {
+	                // Crear carpeta en la ubicación de respaldo
+	                File carpetaNueva = new File(carpetaBackup, archivo.getName());
+	                carpetaNueva.mkdirs();
+	                hacerCopiaDeSeguridad(archivo, carpetaNueva); // Llamada recursiva
+	            } else {
+	                // Copiar archivo
+	                Files.copy(archivo.toPath(), new File(carpetaBackup, archivo.getName()).toPath());
+	            }
+	        }
+	    }
+	}
 
 }
